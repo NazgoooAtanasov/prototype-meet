97ffd57bfdb0e3080bcf1aaf910478f5fe0e7f4b
NazgoooAtanasov <naskodragomirov@gmail.com>
2022-07-23 21:50:41 +0300

    feat: trpc

--- server/index.ts
+++ server/index.ts
@@ -3,10 +3,35 @@ import { signin, signup } from './routes/auth';
 import { getUser, assignLocation } from './routes/users';
 import { auth } from './middlewares/auth';
 import dotenv from 'dotenv';
+import * as trpc from '@trpc/server';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import cors from 'cors';
 
 dotenv.config();
 
 const expressApp = express();
+
+expressApp.use(
+    cors({
+        origin: 'http://localhost:19006',
+    })
+);
+
+const appRouter = trpc.router();
+
+const createContext = ({
+    req,
+    res,
+}: trpcExpress.CreateExpressContextOptions) => ({});
+type Context = trpc.inferAsyncReturnType<typeof createContext>;
+
+expressApp.use(
+    '/trpc',
+    trpcExpress.createExpressMiddleware({
+        router: appRouter,
+        createContext,
+    })
+);
 
 expressApp.use(express.json());
 
@@ -20,3 +45,5 @@ expressApp.post('/auth/signup', signup);
 expressApp.listen('8000', () => {
     console.debug('[server] running at port 8000');
 });
+
+export type AppRouter = typeof appRouter;
