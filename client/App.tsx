53135eedc2b191ef08e04b59dde489d02b3b4148
NazgoooAtanasov <naskodragomirov@gmail.com>
2022-07-26 21:27:28 +0300

    Revert "feat: trpc"
    This reverts commit 97ffd57bfdb0e3080bcf1aaf910478f5fe0e7f4b.

--- client/App.tsx
+++ client/App.tsx
@@ -5,33 +5,18 @@ import Register from './components/Register';
 import Login from './components/Login';
 import Location from './components/Location';
 
-import { trpc } from './client/util/trpc';
-import { useState } from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-
 const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
-    const [queryClient] = useState(() => new QueryClient());
-    const [trpcClient] = useState(() => 
-        trpc.createClient({
-            url: 'http://localhost:8000/trpc'
-        })
-    );
-    
     return (
-        <trpc.Provider client={trpcClient} queryClient={queryClient}>
-            <QueryClientProvider client={queryClient}>
-                <NavigationContainer>
-                    <Tab.Navigator>
-                        <Tab.Screen name="Register" component={Register} />
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Register" component={Register} />
 
-                        <Tab.Screen name="Login" component={Login} />
+                <Tab.Screen name="Login" component={Login} />
 
-                        <Tab.Screen name="Location" component={Location} />
-                    </Tab.Navigator>
-                </NavigationContainer>
-            </QueryClientProvider>
-        </trpc.Provider>
+                <Tab.Screen name="Location" component={Location} />
+            </Tab.Navigator>
+        </NavigationContainer>
     );
 }
--- client/client/util/trpc.ts
+++
@@ -1,4 +0,0 @@
-import { createReactQueryHooks } from '@trpc/react';
-import type { AppRouter } from '../../../server/index';
-
-export const trpc = createReactQueryHooks<AppRouter>();
